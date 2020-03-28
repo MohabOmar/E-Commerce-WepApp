85d579fc8dbac83776d8c0affc954b69e181cc76
mohamedco96 <mohamedco215@gmail.com>
2020-03-28 15:54:47 +0200

    project Template

---
+++ MAM/web/js/main.js
@@ -0,0 +1,12 @@
+/* slider */
+
+$('.slider-one')
+    .not(".slick-intialized")
+    .slick({
+        autoplay:true,
+        autoplaySpeed:3000,
+        dots:true,
+        dots:true,
+        prevArrow:".site-slider .slider-btn .prev",
+        nextArrow:".site-slider .slider-btn .next",
+});
