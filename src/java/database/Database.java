3845409a4fee5401126243aa181f0207d222422e
MohabOmar <mohabomar15@gmail.com>
2020-04-01 23:33:39 +0200

    Retrieving products from database and listing them

--- src/java/database/Database.java
+++ src/java/database/Database.java
@@ -2,41 +2,36 @@ package database;
 
 import Database_Tables.Users;
 import java.sql.*;
+import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-
+import products.Product;
 
 public class Database {
+
     private final String url = "jdbc:postgresql://localhost:5432/oshop";
     private final String user = "postgres";
-    private final String password = "amrwsk13";
+    private final String password = "postgres";
 
     private Connection connection = null;
     private PreparedStatement preparedStatment = null;
     private ResultSet result = null;
     private String sqlCommand;
-        
+
     boolean operation = false;
-    
-    private void connect ()
-    {
-        try
-        {
+
+    private void connect() {
+        try {
             Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection(url,user,password);
+            connection = DriverManager.getConnection(url, user, password);
             System.out.println("Connection is made successfully");
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
-    
-    public boolean addAdmin (Users user)
-    {
-        try
-        {
+
+    public boolean addAdmin(Users user) {
+        try {
             connect();
             sqlCommand = "SELECT createadmin (?,?,?,?,?,?,?,?,?)";
             preparedStatment = connection.prepareStatement(sqlCommand);
@@ -49,30 +44,22 @@ public class Database {
             preparedStatment.setString(7, user.getEmail());
             preparedStatment.setString(8, user.getAddress());
             preparedStatment.setString(9, user.getInterests());
-            
+
             result = preparedStatment.executeQuery();
-            
-            while (result.next())
-            {
+
+            while (result.next()) {
                 operation = result.getBoolean(1);
             }
-        }
-        catch (SQLException ex)
-        {
+        } catch (SQLException ex) {
             ex.printStackTrace();
-        }
-        finally
-        {
+        } finally {
             stop();
             return operation;
         }
     }
-    
-    
-    public boolean addClient (Users user)
-    {
-        try
-        {
+
+    public boolean addClient(Users user) {
+        try {
             connect();
             sqlCommand = "SELECT createclient (?,?,?,?,?,?,?,?,?)";
             preparedStatment = connection.prepareStatement(sqlCommand);
@@ -85,162 +72,142 @@ public class Database {
             preparedStatment.setString(7, user.getEmail());
             preparedStatment.setString(8, user.getAddress());
             preparedStatment.setString(9, user.getInterests());
-            
+
             result = preparedStatment.executeQuery();
-            
-            while (result.next())
-            {
+
+            while (result.next()) {
                 operation = result.getBoolean(1);
             }
-        }
-        catch (SQLException ex)
-        {
+        } catch (SQLException ex) {
             ex.printStackTrace();
-        }
-        finally
-        {
+        } finally {
             stop();
             return operation;
         }
     }
-    
-    public boolean checkLogin (Users user)
-    {
-        try
-        {
+
+    public boolean checkLogin(Users user) {
+        try {
             connect();
             sqlCommand = "SELECT checklogin (?,?)";
-            
+
             preparedStatment = connection.prepareStatement(sqlCommand);
             preparedStatment.setString(1, user.getUNameOrEmail());
             preparedStatment.setString(2, user.getPassword());
             result = preparedStatment.executeQuery();
-            
-            while (result.next())
-            {
+
+            while (result.next()) {
                 operation = result.getBoolean(1);
             }
-        }
-        catch (SQLException ex)
-        {
+        } catch (SQLException ex) {
             ex.printStackTrace();
-        }
-        finally
-        {
+        } finally {
             stop();
             return operation;
         }
     }
-    
-    public boolean checkIsAdmin (Users user)
-    {
-        try
-        {
+
+    public boolean checkIsAdmin(Users user) {
+        try {
             connect();
             sqlCommand = "SELECT checkisadmin (?)";
-            
+
             preparedStatment = connection.prepareStatement(sqlCommand);
             preparedStatment.setString(1, user.getUNameOrEmail());
             result = preparedStatment.executeQuery();
-            
-            while (result.next())
-            {
+
+            while (result.next()) {
                 operation = result.getBoolean(1);
             }
-        }
-        catch (SQLException ex)
-        {
+        } catch (SQLException ex) {
             ex.printStackTrace();
-        }
-        finally
-        {
+        } finally {
             stop();
             return operation;
         }
-    }        
-    
-    public int numberOfCategories ()
-    {
+    }
+
+    public int numberOfCategories() {
         int number = 17;
-        try
-        {
+        try {
             connect();
             sqlCommand = "SELECT COUNT(*) FROM category";
             preparedStatment = connection.prepareStatement(sqlCommand);
             result = preparedStatment.executeQuery();
-            while(result.next())
-            {
+            while (result.next()) {
                 number = result.getInt(1);
             }
-        }
-        catch (SQLException ex)
-        {
+        } catch (SQLException ex) {
             ex.printStackTrace();
-        }
-        finally
-        {
+        } finally {
             return number;
         }
     }
-    
-    public int numberOfUsers ()
-    {
+
+    public int numberOfUsers() {
         int number = 17;
-        try
-        {
+        try {
             connect();
             sqlCommand = "SELECT COUNT(*) FROM users";
             preparedStatment = connection.prepareStatement(sqlCommand);
             result = preparedStatment.executeQuery();
-            while(result.next())
-            {
+            while (result.next()) {
                 number = result.getInt(1);
             }
-        }
-        catch (SQLException ex)
-        {
+        } catch (SQLException ex) {
             ex.printStackTrace();
-        }
-        finally
-        {
+        } finally {
             return number;
         }
-    }    
-    
-    public int numberOfSuccessCart ()
-    {
+    }
+
+    public int numberOfSuccessCart() {
         int number = 17;
-        try
-        {
+        try {
             connect();
             sqlCommand = "SELECT COUNT(*) FROM usercart WHERE issubmitted = true";
             preparedStatment = connection.prepareStatement(sqlCommand);
             result = preparedStatment.executeQuery();
-            while(result.next())
-            {
+            while (result.next()) {
                 number = result.getInt(1);
             }
-        }
-        catch (SQLException ex)
-        {
+        } catch (SQLException ex) {
             ex.printStackTrace();
-        }
-        finally
-        {
+        } finally {
             return number;
         }
-    }    
-        
-    
-    private void stop()
-    {
-        try 
-        {
+    }
+
+    public Vector<Product> retrieveAllProducts() {
+        try {
+            connect();
+            Vector<Product> products = new Vector();
+            sqlCommand = "select * from products";
+            preparedStatment = connection.prepareStatement(sqlCommand);
+            result = preparedStatment.executeQuery();
+            while (result.next()) {
+                products.add(new Product(result.getInt(1),
+                        result.getInt(2),
+                        result.getString(3),
+                        result.getFloat(4),
+                        result.getInt(5),
+                        result.getString(6),
+                        result.getString(7),
+                        result.getBoolean(8)));
+            }
+            return products;
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+
+    private void stop() {
+        try {
             connection.close();
             System.out.println("Database is stopped");
-        } 
-        catch (SQLException ex) 
-        {
+        } catch (SQLException ex) {
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
---
+++ src/java/products/Product.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package products;
+
+/**
+ *
+ * @author MohabOmar
+ */
+public class Product {
+
+    private int productKey;
+    private int categoryId;
+    private String productName;
+    private float price;
+    private int quantity;
+    private String desc;
+    private String img;
+    private boolean isAvail;
+
+    public Product(int productKey, int categoryId, String productName, float price, int quantity, String desc, String img, boolean isAvail) {
+        this.productKey = productKey;
+        this.categoryId = categoryId;
+        this.productName = productName;
+        this.price = price;
+        this.quantity = quantity;
+        this.desc = desc;
+        this.img = img;
+        this.isAvail = isAvail;
+    }
+
+
+    public int getProductKey() {
+        return productKey;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public boolean isIsAvail() {
+        return isAvail;
+    }
+
+    public void setProductKey(int productKey) {
+        this.productKey = productKey;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public void setIsAvail(boolean isAvail) {
+        this.isAvail = isAvail;
+    }
+    
+    
+
+    
+}
